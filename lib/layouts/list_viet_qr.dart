4f1adb11b199256a89d2e0c9778d5bad5a19649a
cuong <cuongtvk@vietqr.com>
2023-09-17 20:14:12 +0700

    update ui

--- lib/layouts/list_viet_qr.dart
+++ lib/layouts/list_viet_qr.dart
@@ -25,11 +25,11 @@ class ListVietQr extends StatelessWidget {
       child: LayoutBuilder(builder: (context, constraints) {
         print(
             '----------------------------${constraints.maxWidth / constraints.maxHeight}');
-        print('----------------------------${constraints.maxWidth}');
+        print('----------------------------${constraints.maxHeight}');
         return Container(
           width: width,
           margin: const EdgeInsets.symmetric(vertical: 8),
-          padding: const EdgeInsets.only(bottom: 20, left: 32, right: 32),
+          padding: const EdgeInsets.only(bottom: 20),
           child: Column(
             children: [
               Expanded(
@@ -40,7 +40,8 @@ class ListVietQr extends StatelessWidget {
                         qrGeneratedDTOs[index],
                         context,
                         (constraints.maxWidth / constraints.maxHeight) > 1.4,
-                        constraints.maxWidth < 700);
+                        constraints.maxWidth < 700,
+                        constraints.maxHeight);
                   }).toList(),
                   options: CarouselOptions(
                     viewportFraction: 1,
@@ -88,9 +89,10 @@ class ListVietQr extends StatelessWidget {
   }
 
   Widget _buildItemQR(QRGeneratedDTO qrGeneratedDTO, BuildContext context,
-      bool horizontalRotation, bool forMobile) {
+      bool horizontalRotation, bool forMobile, double maxHeight) {
     if (horizontalRotation) {
-      return _buildHorizontalRotation(qrGeneratedDTO, context, forMobile);
+      return _buildHorizontalRotation(
+          qrGeneratedDTO, context, forMobile, maxHeight);
     }
     return Column(
       children: [
@@ -187,14 +189,33 @@ class ListVietQr extends StatelessWidget {
     );
   }
 
-  Widget _buildHorizontalRotation(
-      QRGeneratedDTO qrGeneratedDTO, BuildContext context, bool forMobileWeb) {
+  Widget _buildHorizontalRotation(QRGeneratedDTO qrGeneratedDTO,
+      BuildContext context, bool forMobileWeb, double maxHeight) {
+    double sizeLogoVietQr = 0;
+    double sizeLogoNapas = 0;
+    double sizeLogoBank = 0;
+    if (maxHeight < 250) {
+      sizeLogoVietQr = 80;
+      sizeLogoNapas = 60;
+      sizeLogoBank = 60;
+    } else {
+      if (forMobileWeb) {
+        sizeLogoNapas = MediaQuery.of(context).size.width * 0.09;
+        sizeLogoVietQr = MediaQuery.of(context).size.width * 0.12;
+        sizeLogoBank = MediaQuery.of(context).size.width * 0.09;
+      } else {
+        sizeLogoNapas = MediaQuery.of(context).size.width * 0.12;
+        sizeLogoBank = MediaQuery.of(context).size.width * 0.09;
+        sizeLogoVietQr = MediaQuery.of(context).size.height * 0.15;
+      }
+    }
+
     return Row(
       children: [
         Expanded(
           child: Container(
-            margin:
-                const EdgeInsets.only(left: 8, right: 8, top: 24, bottom: 20),
+            margin: const EdgeInsets.all(4),
+            alignment: Alignment.center,
             decoration: BoxDecoration(
               color: AppColor.WHITE,
               borderRadius: const BorderRadius.all(Radius.circular(16)),
@@ -207,62 +228,14 @@ class ListVietQr extends StatelessWidget {
                 ),
               ],
             ),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 16),
-                  child: Image.asset(
-                    'assets/images/ic-viet-qr.png',
-                    width: forMobileWeb
-                        ? MediaQuery.of(context).size.width * 0.12
-                        : MediaQuery.of(context).size.height * 0.15,
-                  ),
-                ),
-                const SizedBox(height: 4),
-                Expanded(
-                  child: QrImage(
-                    data: qrGeneratedDTO.qrCode,
-                    version: QrVersions.auto,
-                    embeddedImage:
-                        const AssetImage('assets/images/ic-viet-qr-small.png'),
-                    embeddedImageStyle: QrEmbeddedImageStyle(
-                      size: const Size(26, 26),
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 24),
-                      child: Image.asset(
-                        'assets/images/ic-napas247.png',
-                        width: forMobileWeb
-                            ? MediaQuery.of(context).size.width * 0.10
-                            : MediaQuery.of(context).size.height * 0.12,
-                      ),
-                    ),
-                    if (qrGeneratedDTO.imgId.isNotEmpty)
-                      Padding(
-                        padding: const EdgeInsets.only(right: 12),
-                        child: Image(
-                          image: ImageUtils.instance
-                              .getImageNetWork(qrGeneratedDTO.imgId),
-                          width: forMobileWeb
-                              ? MediaQuery.of(context).size.width * 0.10
-                              : MediaQuery.of(context).size.height * 0.12,
-                          fit: BoxFit.fill,
-                        ),
-                      )
-                    else
-                      SizedBox(
-                          width: forMobileWeb
-                              ? MediaQuery.of(context).size.width * 0.10
-                              : MediaQuery.of(context).size.height * 0.12),
-                  ],
-                ),
-                const SizedBox(height: 16),
-              ],
+            child: QrImage(
+              data: qrGeneratedDTO.qrCode,
+              version: QrVersions.auto,
+              embeddedImage:
+                  const AssetImage('assets/images/ic-viet-qr-small.png'),
+              embeddedImageStyle: QrEmbeddedImageStyle(
+                size: const Size(26, 26),
+              ),
             ),
           ),
         ),
@@ -273,27 +246,53 @@ class ListVietQr extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                qrGeneratedDTO.userBankName.toUpperCase(),
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontWeight: FontWeight.normal,
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Image.asset(
+                  'assets/images/ic-viet-qr.png',
+                  width: sizeLogoVietQr,
                 ),
               ),
               const SizedBox(height: 4),
               Text(
+                qrGeneratedDTO.userBankName.toUpperCase(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: maxHeight < 250 ? 11 : 14),
+              ),
+              const SizedBox(height: 4),
+              Text(
                 qrGeneratedDTO.bankAccount,
-                style: const TextStyle(
-                  fontWeight: FontWeight.w600,
-                ),
+                style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: maxHeight < 250 ? 11 : 14),
               ),
               const SizedBox(height: 4),
               Text(
                 qrGeneratedDTO.bankName,
                 textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontWeight: FontWeight.normal,
-                ),
+                style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: maxHeight < 250 ? 11 : 14),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Image.asset(
+                    'assets/images/ic-napas247.png',
+                    width: sizeLogoNapas,
+                  ),
+                  if (qrGeneratedDTO.imgId.isNotEmpty)
+                    Image(
+                      image: ImageUtils.instance
+                          .getImageNetWork(qrGeneratedDTO.imgId),
+                      width: sizeLogoBank,
+                      fit: BoxFit.fill,
+                    )
+                  else
+                    SizedBox(width: sizeLogoBank),
+                ],
               ),
             ],
           ),
